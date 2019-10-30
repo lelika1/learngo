b35c9a380ccd472120099882ab1177b11b772c68
Olga Sycheva <lelika1@gmail.com>
2019-10-30 14:33:30 +0100

    Added String function or FS.

--- internal/fs/tree.go
+++ internal/fs/tree.go
@@ -1,6 +1,7 @@
 package fs
 
 import (
+	"sort"
 	"strings"
 )
 
@@ -13,79 +14,88 @@ type Tree struct {
 func NewTree() *Tree {
 	var tree Tree
 	tree.root = &Entry{Name: "", Directory: true}
+	tree.root.children = make(map[string]*Entry)
 	return &tree
+}
+
+// NormalizePath delete redundant /
+func NormalizePath(path string) string {
+	path = strings.TrimSpace(path)
+	for i := strings.Index(path, "//"); i != -1; i = strings.Index(path, "//") {
+		path = path[:i] + path[i+1:]
+	}
+
+	if path[len(path)-1] == '/' {
+		path = path[:len(path)-1]
+	}
+	return path
 }
 
 // AddFile adds a new file with the given path and content to a file tree.
 // Returns false if the file with the given path already exists.
 func (t *Tree) AddFile(path, content string) (ok bool) {
-	if !strings.HasSuffix(path, ".txt") || path[0] != '/' {
+	if len(path) == 0 || path[0] != '/' {
 		return false
 	}
+	pathPart := strings.Split(NormalizePath(path), "/")
 
-	pathPart := strings.Split(path, "/")
-
-	existFolder := 0
-	lastCommon := t.root
-	for ; existFolder < len(pathPart); existFolder++ {
-		for _, child := range lastCommon.children {
-			if child.Name == pathPart[existFolder] {
-				lastCommon = child
-				break
+	i := 1
+	curFold := t.root
+	for ; i < len(pathPart); i++ {
+		child, ok := curFold.children[pathPart[i]]
+		if ok {
+			if !child.Directory {
+				return false // where is a file in the middle of path
 			}
-		}
-		if lastCommon.Name != pathPart[existFolder] {
+			curFold = child
+		} else {
 			break
 		}
 	}
-	if existFolder == len(pathPart) {
-		return false // file was found
+
+	if i == len(pathPart) {
+		return false // file already exist in file system
 	}
-	if existFolder < len(pathPart) {
-		for i := existFolder; i < len(pathPart)-1; i++ {
-			newFold := &Entry{
-				Name:      pathPart[i],
-				Directory: true,
-			}
-			lastCommon.children = append(lastCommon.children, newFold)
-			lastCommon = newFold
+
+	for j := i; j < len(pathPart)-1; j++ {
+		newFold := &Entry{
+			Name:      pathPart[j],
+			Directory: true,
 		}
-		newFile := &Entry{
-			Name:    pathPart[len(pathPart)-1],
-			content: content,
-		}
-		lastCommon.children = append(lastCommon.children, newFile)
+		newFold.children = make(map[string]*Entry)
+		curFold.children[pathPart[j]] = newFold
+		curFold = newFold
 	}
+
+	newFile := &Entry{
+		Name:    pathPart[len(pathPart)-1],
+		content: content,
+	}
+	curFold.children[pathPart[len(pathPart)-1]] = newFile
 
 	return true
 }
 
 // ReadFile returns content for a given file by its path.
-// Returns false if the file does not exist.
+// Returns false if the file does not exist or format is incorrect.
 func (t *Tree) ReadFile(path string) (content string, ok bool) {
-	if !strings.HasSuffix(path, ".txt") || path[0] != '/' {
-		return "s", false
+	if len(path) == 0 || path[0] != '/' {
+		return "", false
 	}
 
-	pathPart := strings.Split(path, "/")
-
-	existFolder := 0
-	lastCommon := t.root
-	for ; existFolder < len(pathPart); existFolder++ {
-		for _, child := range lastCommon.children {
-			if child.Name == pathPart[existFolder] {
-				lastCommon = child
-				break
-			}
-		}
-		if lastCommon.Name != pathPart[existFolder] {
-			break
+	pathPart := strings.Split(NormalizePath(path), "/")
+	curFold := t.root
+	for i := 1; i < len(pathPart); i++ {
+		if child, ok := curFold.children[pathPart[i]]; ok {
+			curFold = child
+		} else {
+			return "", false
 		}
 	}
-	if existFolder == len(pathPart) {
-		return lastCommon.content, true // file was found
+	if curFold.Directory {
+		return "", false
 	}
-	return "s", false
+	return curFold.content, true
 }
 
 // Entry describes one directory entry (either a file or a nested directory).
@@ -94,7 +104,7 @@ type Entry struct {
 	Name      string
 	Directory bool // if this is not a file, but a directory
 	content   string
-	children  []*Entry
+	children  map[string]*Entry
 }
 
 func (e *Entry) String() string {
@@ -110,22 +120,90 @@ func (e *Entry) String() string {
 // Returns false if the dir does not exist or has wrong format.
 // Example: ListDir("/foo/bar") -> [{"foo.txt"}, {"bar.txt"}, {"nested_dir", dir=true}]
 func (t *Tree) ListDir(dir string) (entries []*Entry, ok bool) {
-	if strings.HasSuffix(dir, ".txt") || dir[0] != '/' {
-		return []*Entry{}, false
+	if len(dir) == 0 || dir[0] != '/' {
+		return nil, false
 	}
 
-	pathPart := strings.Split(dir, "/")
-	lastCommon := t.root
-	for i := 0; i < len(pathPart); i++ {
-		for _, child := range lastCommon.children {
-			if child.Name == pathPart[i] {
-				lastCommon = child
-				break
-			}
-		}
-		if lastCommon.Name != pathPart[i] {
-			return []*Entry{}, false
+	pathPart := strings.Split(NormalizePath(dir), "/")
+	curFold := t.root
+	for i := 1; i < len(pathPart); i++ {
+		if child, ok := curFold.children[pathPart[i]]; ok {
+			curFold = child
+		} else {
+			return nil, false
 		}
 	}
-	return lastCommon.children, true
+	if curFold != t.root && !curFold.Directory {
+		return nil, false
+	}
+
+	return sorted(curFold.children), true
+}
+
+func sorted(entries map[string]*Entry) (result []*Entry) {
+	for _, v := range entries {
+		result = append(result, v)
+	}
+
+	sort.Slice(result, func(i, j int) bool { return result[i].Name < result[j].Name })
+	return result
+}
+
+func (t *Tree) String() string {
+	var sb strings.Builder
+	type folderInStack struct {
+		name  string
+		edges []bool
+		desc  *Entry
+	}
+	var stack []folderInStack
+	stack = append(stack, folderInStack{"root", []bool{}, t.root})
+	for len(stack) != 0 {
+		curFold := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		for i := 0; i < len(curFold.edges)-1; i++ {
+			if curFold.edges[i] {
+				sb.WriteString("|  ")
+			} else {
+				sb.WriteString("   ")
+			}
+		}
+		if len(curFold.edges) != 0 {
+			sb.WriteString("|\n")
+		}
+
+		for i := 0; i < len(curFold.edges)-1; i++ {
+			if curFold.edges[i] {
+				sb.WriteString("|  ")
+			} else {
+				sb.WriteString("   ")
+			}
+		}
+		if len(curFold.edges) != 0 {
+			if !curFold.edges[len(curFold.edges)-1] {
+				sb.WriteString("\\__")
+			} else {
+				sb.WriteString("|__")
+			}
+		}
+		if curFold.desc.Directory {
+			sb.WriteString("📁")
+		}
+		sb.WriteString(curFold.name + "\n")
+
+		if curFold.desc.Directory {
+			nested := sorted(curFold.desc.children)
+			for i := len(nested) - 1; i >= 0; i-- {
+				edges := make([]bool, len(curFold.edges))
+				copy(edges, curFold.edges)
+				edges = append(edges, i != len(nested)-1)
+				stack = append(stack, folderInStack{
+					name:  nested[i].Name,
+					edges: edges,
+					desc:  nested[i],
+				})
+			}
+		}
+	}
+	return sb.String()
 }
--- internal/fs/tree_test.go
+++ internal/fs/tree_test.go
@@ -1,6 +1,7 @@
 package fs_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/lelika1/learngo/internal/fs"
@@ -14,11 +15,14 @@ func TestAddFile(t *testing.T) {
 		// "?" for failed one
 		want bool
 	}{
-		{"/foo/boo/foo.txt", "fooboofootxt", true},
 		{"/foo.txt", "footxt", true},
-		{"/foo.txt", "footxt1", false},      // file already exist
-		{"/foo/boo", "fooboo", false},       // incorrect path
+		{"/foo.txt", "footxt1", false}, // file already exist
+		{"/foo/boo/foo.txt", "fooboofootxt", true},
+		{"/foo/boo", "fooboo", false},       // there is folder with name boo
 		{"foo/boo.txt", "foobootxt", false}, // incorrect path
+		{"/foo/boo/foo", "fooboofoo", true},
+		{"/foo/boo/foo/txt.txt", "fooboofootxttxt", false}, // part of path is a file
+		{"", "d", false},                                   // incorrect path
 	}
 	for _, test := range tests {
 		got := fileSys.AddFile(test.path, test.content)
@@ -34,6 +38,7 @@ func TestReadFile(t *testing.T) {
 	fileSys.AddFile("/foo/foo1.txt", "foofoo1txt")
 	fileSys.AddFile("/foo/boo/foo.txt", "fooboofootxt")
 	fileSys.AddFile("/foo.txt", "footxt")
+	fileSys.AddFile("/bar.txt/bar", "bartxtbar")
 
 	tests := []struct {
 		path string
@@ -43,9 +48,15 @@ func TestReadFile(t *testing.T) {
 		{"/foo/boo/foo.txt", "fooboofootxt"},
 		{"/foo.txt", "footxt"},
 		{"/foo/foo1.txt", "foofoo1txt"},
-		{"/foo/boo/foo", "?"},
-		{"/foo/foo11.txt", "?"},
-		{"/", "?"},
+		{"/bar.txt/bar", "bartxtbar"},
+		{"/foo/boo/foo", "?"},   // file doesn't exist
+		{"/foo/foo11.txt", "?"}, // file doesn't exist
+		{"/foo", "?"},           // read folder not file
+		{"/", "?"},              // read folder not file
+		{"/bar.txt", "?"},       // read folder not file
+		{"/bar.txt/ bar", "?"},  //  incorrect path
+		{"bar.txt/bar", "?"},    //  incorrect path
+		{"", "?"},               //  incorrect path
 	}
 	for _, test := range tests {
 		got, ok := fileSys.ReadFile(test.path)
@@ -64,22 +75,29 @@ func TestReadFile(t *testing.T) {
 
 func TestListDir(t *testing.T) {
 	fileSys := fs.NewTree()
-	fileSys.AddFile("/foo/foo.txt", "foofootxt")
-	fileSys.AddFile("/foo/bar.txt", "foofoo1txt")
-	fileSys.AddFile("/foo/bar/foo.txt", "fooboofootxt")
 	fileSys.AddFile("/foo.txt", "footxt")
+	fileSys.AddFile("/foo/foo.txt", "foofootxt")
+	fileSys.AddFile("/foo/aaa.txt", "fooaaatxt")
+	fileSys.AddFile("/foo/boo/foo.txt", "fooboofootxt")
+	fileSys.AddFile("/foo/bar.txt", "foofoo1txt")
+	fileSys.AddFile("/foo/bar/bar", "foobarbar")
+	fileSys.AddFile("/foo/bar/foo.txt", "foobarfootxt")
+	fileSys.AddFile("/bar.txt/bar", "bartxtbar")
 
 	tests := []struct {
 		dir string
 		// Format: "[{"foo.txt"}, {"nested_dir", dir=true}]" for successful result; "?" for failed one.
 		want string
 	}{
-		{"/foo/bar", "[{\"foo.txt\"}]"},
-		{"/foo", "[{\"foo.txt\"}, {\"bar.txt\"}, {\"bar\", dir=true}]"},
-		{"/", "[{\"foo\", dir=true}, {\"foo.txt\"}]"},
-		{"/foo.txt", "?"},
-		{"foo/bar/bar2", "?"},
-		{"/foo/foo1", "?"},
+		{"/foo/bar", "[{\"bar\"}, {\"foo.txt\"}]"},
+		{"/foo", "[{\"aaa.txt\"}, {\"bar\", dir=true}, {\"bar.txt\"}, {\"boo\", dir=true}, {\"foo.txt\"}]"},
+		{"/", "[{\"bar.txt\", dir=true}, {\"foo\", dir=true}, {\"foo.txt\"}]"},
+		{"/bar.txt///", "[{\"bar\"}]"},
+		{"foo/bar/bar2", "?"}, // incorrect path
+		{"/bar.txt/bar", "?"}, // it is a file, not a folder
+		{"/foo.txt", "?"},     // it is a file, not a folder
+		{"/foo/foo1", "?"},    // folder doesn't exist
+		{"", "?"},             // incorrect path
 	}
 	for _, test := range tests {
 		entries, ok := fileSys.ListDir(test.dir)
@@ -102,4 +120,44 @@ func TestListDir(t *testing.T) {
 			t.Errorf("ListDir(%v) = (%v:%v), want %v", test.dir, got, ok, test.want)
 		}
 	}
+}
+
+func TestNormalizeString(t *testing.T) {
+
+	tests := []struct {
+		path string
+		want string
+	}{
+		{"/foo/bar", "/foo/bar"},
+		{"/foo///bar", "/foo/bar"},
+		{"//foo///bar", "/foo/bar"},
+		{"/foo///bar", "/foo/bar"},
+		{"/foo///bar/////", "/foo/bar"},
+		{"/foo///bar/////    ", "/foo/bar"},
+		{"/foo/bar     ", "/foo/bar"},
+		{"/////foo     ", "/foo"},
+		{"///", ""},
+		{"///   ", ""},
+		{"//", ""},
+		{"/", ""},
+	}
+	for _, test := range tests {
+		got := fs.NormalizePath(test.path)
+		if got != test.want {
+			t.Errorf("NormalizePath(%v) = %v, want %v", test.path, got, test.want)
+		}
+	}
+}
+
+func TestString(t *testing.T) {
+	fileSys := fs.NewTree()
+	fileSys.AddFile("/one/new/few", "")
+	fileSys.AddFile("/one/new/many", "")
+	fileSys.AddFile("/one/old/few", "")
+	fileSys.AddFile("/one/old/many", "")
+	fileSys.AddFile("/two/new/few", "")
+	fileSys.AddFile("/two/new/many", "")
+	fileSys.AddFile("/two/old/few", "")
+	fileSys.AddFile("/two/old/many", "")
+	fmt.Println(fileSys)
 }
